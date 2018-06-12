b38684a579f6c5422158f05b65897a1fbdce455e
Sean Kim <schongkim@rkci.com>
2018-06-12 17:04:05 -0500

    updated ExtentManager.cs to use klov reporter feature

--- RKCIUIAutomation/Base/ExtentManager.cs
+++ RKCIUIAutomation/Base/ExtentManager.cs
@@ -10,14 +10,25 @@ namespace RKCIUIAutomation.Base
     {
         public static readonly string reportFilePath = $"{extentReportPath}\\extent.html";
         private static readonly Lazy<ExtentReports> _lazy = new Lazy<ExtentReports>(() => new ExtentReports());
+        private static readonly Lazy<KlovReporter> _lazyKlov = new Lazy<KlovReporter>(() => new KlovReporter());
 
         public static ExtentReports Instance { get { return _lazy.Value; } }
+        public static KlovReporter Klov { get { return _lazyKlov.Value; } }
 
         static ExtentManager()
         {            
             ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(reportFilePath);
             htmlReporter.LoadConfig($"{GetCodeBasePath()}\\extent-config.xml");
-            Instance.AttachReporter(htmlReporter);
+
+            if (BaseClass.testPlatform != Config.TestPlatform.Local)
+            {
+                Klov.InitMongoDbConnection("localhost", 27017);
+                Klov.ProjectName = "RKCIUIAutomation";
+                Klov.ReportName = "RKCI Elvis UI Automation";
+                Klov.KlovUrl = "http://localhost:8088";
+                Instance.AttachReporter(htmlReporter, Klov);
+            } else
+                Instance.AttachReporter(htmlReporter);
         }
 
         private ExtentManager() { }
