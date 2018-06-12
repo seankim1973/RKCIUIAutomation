126a9fd3572862c6f6a2254c9cd54476e219d033
Sean Kim <schongkim@rkci.com>
2018-06-12 17:32:39 -0500

    removed klovReporter

--- RKCIUIAutomation/Base/ExtentManager.cs
+++ RKCIUIAutomation/Base/ExtentManager.cs
@@ -10,26 +10,14 @@ namespace RKCIUIAutomation.Base
     {
         public static readonly string reportFilePath = $"{extentReportPath}\\extent.html";
         private static readonly Lazy<ExtentReports> _lazy = new Lazy<ExtentReports>(() => new ExtentReports());
-        //private static readonly Lazy<KlovReporter> _lazyKlov = new Lazy<KlovReporter>(() => new KlovReporter());
 
         public static ExtentReports Instance { get { return _lazy.Value; } }
-        //public static KlovReporter Klov { get { return _lazyKlov.Value; } }
 
         static ExtentManager()
         {            
             ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(reportFilePath);
             htmlReporter.LoadConfig($"{GetCodeBasePath()}\\extent-config.xml");
-
-            if (BaseClass.testPlatform != Config.TestPlatform.Local)
-            {
-                KlovReporter Klov = new KlovReporter();
-                Klov.InitMongoDbConnection("localhost", 27017);
-                Klov.ProjectName = "RKCIUIAutomation";
-                Klov.ReportName = "RKCI Elvis UI Automation";
-                Klov.KlovUrl = "http://localhost:8088";
-                Instance.AttachReporter(htmlReporter, Klov);
-            } else
-                Instance.AttachReporter(htmlReporter);
+            Instance.AttachReporter(htmlReporter);
         }
 
         private ExtentManager() { }
