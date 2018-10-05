89f98fde2f99eecddc943a469a034b9cdd80d0ff
Sean Kim <schongkim@rkci.com>
2018-10-05 15:25:07 -0500

    Added required field error labels verification. Added QaRcrdCtrl_GeneralNCR_WF class in Workflows folder. Moved QA Record Control unit tests into separate class.

---
+++ RKCIUIAutomation/Test/QaRcrdCtrl_GeneralNCR_Test.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using RKCIUIAutomation.Config;
+using RKCIUIAutomation.Page.PageObjects.QARecordControl;
+using static RKCIUIAutomation.Page.PageObjects.QARecordControl.GeneralNCR;
+
+namespace RKCIUIAutomation.Test.NCR
+{
+    [TestFixture]
+    public class Verify_Create_And_Save_NCR_Document : TestBase
+    {
+        [Test]
+        [Category(Component.NCR)]
+        [Property(TestCaseNumber, 2187687)]
+        [Property(Priority, "High")]
+        [Description("To validate successful create and save of an NCR (Nonconformance Report) document.")]
+        public void Create_And_Save_NCR_Document()
+        {
+            WF_QaRcrdCtrl_GeneralNCR.CreateAndSaveForwardNCRDocument(UserType.NCRTech);
+            Assert.True(QaRcrdCtrl_GeneralNCR.VerifyNCRDocIsDisplayed(TableTab.CQM_Review));
+            LogoutToLoginPage();
+        }
+    }
+
+    [TestFixture]
+    public class Verify_QC_Review_of_NCR_document_by_NCR_Manager : TestBase
+    {
+        [Test]
+        [Category(Component.NCR)]
+        [Property(TestCaseNumber, 2187688)]
+        [Property(Priority, "High")]
+        [Description("To validate the QC review part of an NCR (Nonconformance Report).")]
+        public void QC_Review_of_NCR_document_by_NCR_Manager()
+        {
+            string ncrDescription = WF_QaRcrdCtrl_GeneralNCR.CreateAndSaveForwardNCRDocument(UserType.NCRTech);
+            LogoutToLoginPage();
+            WF_QaRcrdCtrl_GeneralNCR.ReviewNCRDocument(UserType.NCRMgr, ncrDescription);
+            
+        }
+    }
+
+    [TestFixture]
+    public class Verify_Revising_the_NCR_Document : TestBase
+    {
+        [Test]
+        [Category(Component.NCR)]
+        [Property(TestCaseNumber, 2187689)]
+        [Property(Priority, "High")]
+        [Description("To successfully revising an NCR (Nonconformance Report) document.")]
+        public void Revise_the_NCR_Document()
+        {
+            LoginAs(UserType.NCRMgr);
+            NavigateToPage.QARecordControl_General_NCR();
+        }
+    }
+
+    [TestFixture]
+    public class Verify_Closing_the_NCR_Document : TestBase
+    {
+        [Test]
+        [Category(Component.NCR)]
+        [Property(TestCaseNumber, 2187690)]
+        [Property(Priority, "High")]
+        [Description("To successfully close an NCR (Nonconformance Report) document.")]
+        public void Close_the_NCR_Document()
+        {
+            LoginAs(UserType.NCRMgr);
+            NavigateToPage.QARecordControl_General_NCR();
+        }
+    }
+
+    [TestFixture]
+    public class Verify_Editing_the_NCR_Document : TestBase
+    {
+        [Test]
+        [Category(Component.NCR)]
+        [Property(TestCaseNumber, 2187691)]
+        [Property(Priority, "High")]
+        [Description("To successfully edit an NCR (Nonconformance Report) document.")]
+        public void Edit_the_NCR_Document()
+        {
+            LoginAs(UserType.NCRMgr);
+            NavigateToPage.QARecordControl_General_NCR();
+        }
+    }
+
+    [TestFixture]
+    public class Verify_Vewing_NCR_Document_Report : TestBase
+    {
+        [Test]
+        [Category(Component.NCR)]
+        [Property(TestCaseNumber, 2188063)]
+        [Property(Priority, "High")]
+        [Description("To successfully view the report of an NCR (Nonconformance Report) document.")]
+        public void View_NCR_Document_Report()
+        {
+            LoginAs(UserType.NCRMgr);
+            NavigateToPage.QARecordControl_General_NCR();
+        }
+    }
+}
--- RKCIUIAutomation/Test/UnitTests/QaRcrdCtrl_GeneralNCR_UnitTest.cs
+++ RKCIUIAutomation/Test/UnitTests/QaRcrdCtrl_GeneralNCR_UnitTest.cs
@@ -1,108 +1,11 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using RKCIUIAutomation.Config;
-using RKCIUIAutomation.Page.PageObjects.QARecordControl;
 using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace RKCIUIAutomation.Test.NCR
+namespace RKCIUIAutomation.Test.UnitTests
 {
-    [TestFixture]
-    public class Verify_Create_And_Save_NCR_Document : TestBase
-    {
-        [Test]
-        [Category(Component.NCR)]
-        [Property(TestCaseNumber, 2187687)]
-        [Property(Priority, "High")]
-        [Description("To validate successful create and save of an NCR (Nonconformance Report) document.")]
-        public void Create_And_Save_NCR_Document()
-        {
-            LoginAs(UserType.NCRTech);
-            NavigateToPage.QARecordControl_General_NCR();
-            QaRcrdCtrl_GeneralNCR.ClickBtn_New();
-            QaRcrdCtrl_GeneralNCR.PopulateRequiredFieldsAndSave();
-            Assert.True(QaRcrdCtrl_GeneralNCR.VerifyNCRDocInReviseTab());
-        }
-    }
-
-    [TestFixture]
-    public class Verify_QC_Review_of_NCR_document_by_NCR_Manager : TestBase
-    {
-        [Test]
-        [Category(Component.NCR)]
-        [Property(TestCaseNumber, 2187688)]
-        [Property(Priority, "High")]
-        [Description("To validate the QC review part of an NCR (Nonconformance Report).")]
-        public void QC_Review_of_NCR_document_by_NCR_Manager()
-        {
-            LoginAs(UserType.NCRMgr);
-            NavigateToPage.QARecordControl_General_NCR();
-        }
-    }
-
-    [TestFixture]
-    public class Verify_Revising_the_NCR_Document : TestBase
-    {
-        [Test]
-        [Category(Component.NCR)]
-        [Property(TestCaseNumber, 2187689)]
-        [Property(Priority, "High")]
-        [Description("To successfully revising an NCR (Nonconformance Report) document.")]
-        public void Revise_the_NCR_Document()
-        {
-            LoginAs(UserType.NCRMgr);
-            NavigateToPage.QARecordControl_General_NCR();
-        }
-    }
-
-    [TestFixture]
-    public class Verify_Closing_the_NCR_Document : TestBase
-    {
-        [Test]
-        [Category(Component.NCR)]
-        [Property(TestCaseNumber, 2187690)]
-        [Property(Priority, "High")]
-        [Description("To successfully close an NCR (Nonconformance Report) document.")]
-        public void Close_the_NCR_Document()
-        {
-            LoginAs(UserType.NCRMgr);
-            NavigateToPage.QARecordControl_General_NCR();
-        }
-    }
-
-    [TestFixture]
-    public class Verify_Editing_the_NCR_Document : TestBase
-    {
-        [Test]
-        [Category(Component.NCR)]
-        [Property(TestCaseNumber, 2187691)]
-        [Property(Priority, "High")]
-        [Description("To successfully edit an NCR (Nonconformance Report) document.")]
-        public void Edit_the_NCR_Document()
-        {
-            LoginAs(UserType.NCRMgr);
-            NavigateToPage.QARecordControl_General_NCR();
-        }
-    }
-
-    [TestFixture]
-    public class Verify_Vewing_NCR_Document_Report : TestBase
-    {
-        [Test]
-        [Category(Component.NCR)]
-        [Property(TestCaseNumber, 2188063)]
-        [Property(Priority, "High")]
-        [Description("To successfully view the report of an NCR (Nonconformance Report) document.")]
-        public void View_NCR_Document_Report()
-        {
-            LoginAs(UserType.NCRMgr);
-            NavigateToPage.QARecordControl_General_NCR();
-        }
-    }
-
-
-
     [TestFixture]
     public class UnitTest_NCR_UnitTest_Garnet : TestBase
     {
