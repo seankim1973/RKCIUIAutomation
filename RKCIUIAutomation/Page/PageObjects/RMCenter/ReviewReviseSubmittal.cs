b024b67f6aaa6ec55eea33e562c7a36b73789719
Sean Kim <schongkim@rkci.com>
2019-05-02 13:34:32 -0500

    initial checkin for ReviewReviseSubmittal page object class

--- RKCIUIAutomation/Page/PageObjects/RMCenter/ReviewReviseSubmittal.cs
+++ RKCIUIAutomation/Page/PageObjects/RMCenter/ReviewReviseSubmittal.cs
@@ -1,6 +1,107 @@
-﻿namespace RKCIUIAutomation.Page.PageObjects.RMCenter
+﻿using OpenQA.Selenium;
+using RKCIUIAutomation.Test;
+
+namespace RKCIUIAutomation.Page.PageObjects.RMCenter
 {
-    internal class ReviewReviseSubmittal
+    public class ReviewReviseSubmittal : ReviewReviseSubmittal_Impl
+    {
+        public ReviewReviseSubmittal(IWebDriver driver) => this.Driver = driver;
+    }
+
+    public interface IReviewReviseSubmittal
     {
     }
+
+    public abstract class ReviewReviseSubmittal_Impl : TestBase, IReviewReviseSubmittal
+    {
+    }
+
+
+    #region Implementation specific to Garnet
+
+    public class ReviewReviseSubmittal_Garnet : ReviewReviseSubmittal
+    {
+        public ReviewReviseSubmittal_Garnet(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+
+    #endregion Implementation specific to Garnet
+
+
+    #region Implementation specific to GLX
+
+    public class ReviewReviseSubmittal_GLX : ReviewReviseSubmittal
+    {
+        public ReviewReviseSubmittal_GLX(IWebDriver driver) : base(driver)
+        {
+        }
+
+    }
+
+    #endregion Implementation specific to GLX
+
+
+    #region Implementation specific to SH249
+
+    public class ReviewReviseSubmittal_SH249 : ReviewReviseSubmittal
+    {
+        public ReviewReviseSubmittal_SH249(IWebDriver driver) : base(driver)
+        {
+        }
+
+    }
+
+    #endregion Implementation specific to SH249
+
+
+    #region Implementation specific to SGWay
+
+    public class ReviewReviseSubmittal_SGWay : ReviewReviseSubmittal
+    {
+        public ReviewReviseSubmittal_SGWay(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+    }
+
+    #endregion Implementation specific to SGWay
+
+
+    #region Implementation specific to I15South
+
+    public class ReviewReviseSubmittal_I15South : ReviewReviseSubmittal
+    {
+        public ReviewReviseSubmittal_I15South(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+
+    #endregion Implementation specific to I15South
+
+
+    #region Implementation specific to I15Tech
+
+    public class ReviewReviseSubmittal_I15Tech : ReviewReviseSubmittal
+    {
+        public ReviewReviseSubmittal_I15Tech(IWebDriver driver) : base(driver)
+        {
+        }
+
+    }
+
+    #endregion Implementation specific to I15Tech
+
+
+    #region Implementation specific to LAX
+
+    public class ReviewReviseSubmittal_LAX : ReviewReviseSubmittal
+    {
+        public ReviewReviseSubmittal_LAX(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+
+    #endregion Implementation specific to LAX
 }
